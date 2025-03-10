87ffaf7a5e341c382906635c771005845d0d0e7e
Daniel Firth <daniel.firth@iohk.io>
2025-03-10 20:17:22 +0000

    Use three parties and different scenarios for alice and bob in vm test.

--- nix/test-system.nix
+++ nix/test-system.nix
@@ -11,8 +11,12 @@ let
         services.etcd = {
           advertiseClientUrls = [ "http://alice:2379" ];
           enable = true;
+          extraConf = {
+            "ETCD_AUTO_COMPACTION_RETENTION" = "100";
+            "ETCD_AUTO_COMPACTION_MODE" = "revision";
+          };
           initialAdvertisePeerUrls = [ "http://alice:2380" ];
-          initialCluster = [ "alice=http://alice:2380,bob=http://bob:2380" ];
+          initialCluster = [ "alice=http://alice:2380,bob=http://bob:2380,carol=http://carol:2380" ];
           initialClusterToken = "grapesy-etcd-test";
           listenClientUrls = [ "http://127.0.0.1:2379" ];
           listenPeerUrls = [ "http://0.0.0.0:2380" ];
@@ -30,7 +34,7 @@ let
           advertiseClientUrls = [ "http://bob:2379" ];
           enable = true;
           initialAdvertisePeerUrls = [ "http://bob:2380" ];
-          initialCluster = [ "alice=http://alice:2380,bob=http://bob:2380" ];
+          initialCluster = [ "alice=http://alice:2380,bob=http://bob:2380,carol=http://carol:2380" ];
           initialClusterToken = "grapesy-etcd-test";
           listenClientUrls = [ "http://127.0.0.1:2379" ];
           listenPeerUrls = [ "http://0.0.0.0:2380" ];
@@ -42,17 +46,43 @@ let
           memorySize = 4096;
         };
       };
+      carol = { ... }: {
+        environment.systemPackages = [ pkgs.etcd ];
+        services.etcd = {
+          advertiseClientUrls = [ "http://carol:2379" ];
+          enable = true;
+          initialAdvertisePeerUrls = [ "http://carol:2380" ];
+          initialCluster = [ "alice=http://alice:2380,bob=http://bob:2380,carol=http://carol:2380" ];
+          initialClusterToken = "grapesy-etcd-test";
+          listenClientUrls = [ "http://127.0.0.1:2379" ];
+          listenPeerUrls = [ "http://0.0.0.0:2380" ];
+          name = "carol";
+        };
+        networking.firewall.enable = false;
+        virtualisation = {
+          cores = 2;
+          memorySize = 4096;
+        };
+      };
     };
     testScript = ''
       alice.start()
       bob.start()
+      carol.start()
       alice.wait_for_open_port(2379)
       bob.wait_for_open_port(2379)
+      carol.wait_for_open_port(2379)
       alice.wait_for_open_port(2380)
       bob.wait_for_open_port(2380)
+      carol.wait_for_open_port(2380)
       alice.succeed("etcdctl member list --endpoints http://127.0.0.1:2379")
       bob.succeed("etcdctl member list --endpoints http://127.0.0.1:2379")
-      alice.succeed("${haskellPackages.grapesy-etcd-testing}/bin/tests")
+      carol.succeed("etcdctl member list --endpoints http://127.0.0.1:2379")
+
+      bob.block()
+      alice.succeed("${haskellPackages.grapesy-etcd-testing}/bin/alice")
+      bob.unblock()
+      bob.succeed("${haskellPackages.grapesy-etcd-testing}/bin/bob")
     '';
   };
 
