b7966299aa606e1b1bc545cd3abb87d55f070b49
Daniel Firth <daniel.firth@iohk.io>
2025-03-14 14:44:32 +0000

    Add leaseGrant and leaseKeepAlive to Etcd.Simple

--- grapesy-etcd/src/Network/GRPC/Etcd/Client/Simple.hs
+++ grapesy-etcd/src/Network/GRPC/Etcd/Client/Simple.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Network.GRPC.Etcd.Client.Simple (
+    leaseGrant,
+    leaseKeepAlive,
     put,
     range,
     watch,
@@ -12,15 +14,64 @@ import Network.GRPC.Client (Connection, rpc)
 import Network.GRPC.Client.StreamType.IO (biDiStreaming, nonStreaming)
 import Network.GRPC.Common.NextElem qualified as NextElem
 import Network.GRPC.Common.Protobuf (Proto, Protobuf)
-import Network.GRPC.Etcd (KV, PutRequest, PutResponse, RangeRequest, RangeResponse, Watch, WatchRequest, WatchResponse)
+import Network.GRPC.Etcd (
+    KV,
+    Lease,
+    LeaseGrantRequest,
+    LeaseGrantResponse,
+    LeaseKeepAliveRequest,
+    LeaseKeepAliveResponse,
+    PutRequest,
+    PutResponse,
+    RangeRequest,
+    RangeResponse,
+    Watch,
+    WatchRequest,
+    WatchResponse,
+ )
 
-put :: (MonadIO m) => (MonadMask m) => Connection -> Proto PutRequest -> m (Proto PutResponse)
+leaseGrant ::
+    (MonadIO m) =>
+    (MonadMask m) =>
+    Connection ->
+    Proto LeaseGrantRequest ->
+    m (Proto LeaseGrantResponse)
+leaseGrant conn = nonStreaming conn (rpc @(Protobuf Lease "leaseGrant"))
+
+leaseKeepAlive ::
+    (MonadIO m) =>
+    (MonadMask m) =>
+    Connection ->
+    [Proto LeaseKeepAliveRequest] ->
+    (Proto LeaseKeepAliveResponse -> m ()) ->
+    m ()
+leaseKeepAlive conn reqs resp = biDiStreaming conn (rpc @(Protobuf Lease "leaseKeepAlive")) $ \send recv -> do
+    NextElem.forM_ reqs send
+    NextElem.whileNext_ recv resp
+
+put ::
+    (MonadIO m) =>
+    (MonadMask m) =>
+    Connection ->
+    Proto PutRequest ->
+    m (Proto PutResponse)
 put conn = nonStreaming conn (rpc @(Protobuf KV "put"))
 
-range :: (MonadIO m) => (MonadMask m) => Connection -> Proto RangeRequest -> m (Proto RangeResponse)
+range ::
+    (MonadIO m) =>
+    (MonadMask m) =>
+    Connection ->
+    Proto RangeRequest ->
+    m (Proto RangeResponse)
 range conn = nonStreaming conn (rpc @(Protobuf KV "range"))
 
-watch :: (MonadIO m) => (MonadMask m) => Connection -> [Proto WatchRequest] -> (Proto WatchResponse -> m ()) -> m ()
+watch ::
+    (MonadIO m) =>
+    (MonadMask m) =>
+    Connection ->
+    [Proto WatchRequest] ->
+    (Proto WatchResponse -> m ()) ->
+    m ()
 watch conn reqs resp = biDiStreaming conn (rpc @(Protobuf Watch "watch")) $ \send recv -> do
     NextElem.forM_ reqs send
     NextElem.whileNext_ recv resp
